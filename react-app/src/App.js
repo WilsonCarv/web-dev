443d1548309cc35934d5e87abf1207390980073b
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-04-19 13:35:06 -0600

    use defered value

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { CompositionComponent } from './components/compositionComponent/CompositionComponent';
 import HigherOrderComponent from './components/higherOrderComponent/HigherOrderComponent';
 import ThemeContext from './components/themeContext/ThemeContext';
+import UseDeferredValueComponent from './components/useDeferredValue/UseDeferredValueComponent';
 import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<CompositionComponent />
 				<HigherOrderComponent />
 				<ThemeContext />
+				<UseDeferredValueComponent />
 			</ThemeProvider>
 		</>
 	);
