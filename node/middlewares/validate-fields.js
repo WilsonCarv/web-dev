5f0a136cf89e84fbe0ea73bb2678437693ef883f
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-04-19 17:35:08 -0600

    validate fields

---
+++ node/middlewares/validate-fields.js
@@ -0,0 +1,11 @@
+const { validationResult } = require('express-validator');
+
+const validateFields = (req, resp, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return resp.status(400).json(errors);
+	}
+	next();
+};
+
+module.exports = { validateFields };
--- node/routes/user.routes.js
+++ node/routes/user.routes.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
 const { postUser } = require('../controllers/user.controller');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
-router.post('/', postUser);
+router.post(
+	'/',
+	[
+		check('name', 'The name is required').not().isEmpty(),
+		check('email', 'The email is required').isEmail(),
+		check('password', 'The password is required').isLength({ min: 6 }),
+		validateFields
+	],
+	postUser
+);
 
 module.exports = router;
