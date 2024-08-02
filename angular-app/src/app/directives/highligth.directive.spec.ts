c0aa1c617e80fcba05d0b298e66ba87c6916a166
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-08-02 11:17:33 -0600

    format code

--- angular-app/src/app/directives/highligth.directive.spec.ts
+++ angular-app/src/app/directives/highligth.directive.spec.ts
@@ -2,7 +2,7 @@ import { HighlightDirective } from './highligth.directive';
 
 describe('HighligthDirective', () => {
 	it('should create an instance', () => {
-		const directive = new HighlightDirective();
+		const directive = new HighlightDirective({} as any);
 		expect(directive).toBeTruthy();
 	});
 });
