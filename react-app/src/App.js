6a1d9d4496d20db8e5bb168329889847f74c0ec0
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-08-07 08:51:02 -0600

    move react app to sandbox

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,32 +1,9 @@
 import './App.css';
-import { CompositionComponent } from './components/compositionComponent/CompositionComponent';
-import HigherOrderComponent from './components/higherOrderComponent/HigherOrderComponent';
-import SuspenseComponent from './components/suspense/SuspenseComponent';
-import ThemeContext from './components/themeContext/ThemeContext';
-import UseDeferredValueComponent from './components/useDeferredValue/UseDeferredValueComponent';
-import { ThemeProvider } from './context/ThemeContext';
-import TransitionComponent from './components/transition/TransitionComponent';
-import LazyComponent from './components/lazy/LazyComponent';
-import PortalComponent from './components/portal/PortalComponent';
-import ErrorBoundarySampleComponent from './components/errorBoundaryComponent/ErrorBoundarySampleComponent';
 
 function App() {
 	return (
 		<>
-			<ThemeProvider>
-				<CompositionComponent />
-				<HigherOrderComponent />
-				<ThemeContext />
-				<UseDeferredValueComponent />
-				<SuspenseComponent />
-				<br />
-				<TransitionComponent />
-				<br />
-				<LazyComponent />
-				<br />
-				<PortalComponent />
-				<ErrorBoundarySampleComponent />
-			</ThemeProvider>
+			<h1>React App</h1>
 		</>
 	);
 }
