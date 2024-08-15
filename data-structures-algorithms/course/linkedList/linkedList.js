ba434a7507216bf3f0bbd735c9916fe9c2b70025
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-08-15 12:57:26 -0600

    pop method

--- data-structures-algorithms/course/linkedList/linkedList.js
+++ data-structures-algorithms/course/linkedList/linkedList.js
@@ -35,10 +35,28 @@ class LinkedList {
 		}
 		this.length++;
 	}
+	pop() {
+		if (!this.head) return undefined;
+		let temp = this.head;
+		let pre = this.head;
+		while (temp.next) {
+			pre = temp;
+			temp = temp.next;
+		}
+		this.tail = pre;
+		this.tail.next = null;
+		this.length--;
+		if (this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return temp;
+	}
 }
 
 let myLinkedList = new LinkedList(4);
 myLinkedList.push(5);
 myLinkedList.push(6);
 myLinkedList.push(7);
+console.log(myLinkedList.pop());
 myLinkedList.printList();
