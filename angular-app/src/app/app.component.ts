ac08d4b1f663607e3f49f928e4b9cda95ea05061
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-22 18:25:04 -0600

    add experimental zoneless and try some defer views

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ParentSignalComponent } from './sandbox/components/signal/parent-signal/parent-signal.component';
 import { DeferComponent } from './sandbox/components/defer/defer.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [ParentSignalComponent, DeferComponent],
+	imports: [DeferComponent],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
