fd532a8620249f9d44c2131d23cb5b843540604b
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-11-22 18:40:05 -0600

    resource api

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { DeferComponent } from './sandbox/components/defer/defer.component';
 import { ResourceComponent } from './sandbox/components/resource/resource.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [DeferComponent, ResourceComponent],
+	imports: [ResourceComponent],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
